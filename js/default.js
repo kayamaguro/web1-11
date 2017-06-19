1ab4536cd560451a3a47f0e4e695c1fb96551325
kayamaguro <g1627017@st.t-kougei.ac.jp>
2017-06-19 17:08:58 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,2 +1,17 @@
-//日時を表示
-document.getElementBy
+//答えを用意
+ver number = Math.floor(Math.random()*3);
+ver answer = parseInt(window.prompt('数あてゲーム！0～2の数字を入力してください！'));
+
+ver message;
+if(answer === number){
+message = 'あたり！';
+}else if(answer < number){
+message = '残念！！！もっと大きいです';
+}else if(answer > number){
+message = '残念！！！もっと小さいです';
+}else{
+message ='0～3の数字を入力してください！';
+}
+
+//結果を表示
+document.getElementById('choice').textContent = message;
